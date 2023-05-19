5f58fb5f4199cd889234c862d8d1c306f1be6f0a
Htet-Aung-Shane <mmc.htetaungshane@gmail.com>
2023-05-19 16:00:42 +0630

    update admission

--- admission/models/admission.py
+++ admission/models/admission.py
@@ -1,4 +1,5 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 class Admission(models.Model):
     _name = 'ha.admission'
@@ -11,6 +12,7 @@ class Admission(models.Model):
         self.env['ir.sequence'].next_by_code('has.admission'))
     start_date = fields.Date ('Start Date')
     end_date = fields.Date ('End Date')
+    #diff_date = fields.Char(compute='_diff_date', store=True, string='Diff Date')
     student = fields.Many2one('ha.student', string='Student')
     partner = fields.Many2one('res.partner',related='student.partner', string='Partner')
     course =  fields.Many2one('ha.course', string='Course')
@@ -19,10 +21,18 @@ class Admission(models.Model):
         selection=[
             ('draft', 'Draft'), ('done', 'Done')], default='draft', tracking=True)
     #subject = fields.Many2many('ha.subject', string='Subjects')
+    
+    @api.constrains('start_date', 'end_date')
+    def _diff_date(self):
+        for rec in self:
+            if rec.end_date <= rec.start_date:
+                    print(rec.end_date)
+                    raise ValidationError(
+                        _("End Date Must Greater Than Start Date"))
 
-    @api.model    
     def action_done(self):
         self.state = 'done'
+        self.student.write({'course':self.course})
 
     def action_set_to_draft(self):
         self.state = 'draft'
