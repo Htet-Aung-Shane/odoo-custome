3a052e4a7a31ca6ccffb6512375296c42a9ffd5e
Htet-Aung-Shane <mmc.htetaungshane@gmail.com>
2023-05-25 15:59:02 +0630

    commit for api

--- admission/controllers/enrollment_controller.py
+++ admission/controllers/enrollment_controller.py
@@ -1,9 +1,27 @@
 import odoo.http as http
 from odoo.http import request
+from odoo.http import request, Response
 
 class Enrollment(http.Controller):
     @http.route(['/enrollment'], type='http',
                 auth='public', website=True)
-    def inquiry(self, **kw):
+    def inquiry(self, **post):
         return http.request.render(
             "admission.enrollment_form", post)
+
+    @http.route(['/enrollment/submit'], type='http', auth='public', website=True)
+    def enrollment_submit(self, **post):
+        enrollment_id = False
+        return http.request.render(
+            "admission.enrollment_confirmed", post)
+
+    @http.route(['/enrollment/api'], type='http', auth='public', website=True)
+    def api(self, **kwargs):
+
+        model_name = 'ha.admission'
+        result = {
+            'success': True,
+            'message': 'Ok',
+            }
+        headers = {'Content-Type': 'application/json'}
+        return Response(json.dumps(result), headers=headers)
